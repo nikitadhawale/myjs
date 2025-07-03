89ce3834370687813e8c73e1bcf998febf1318e3
nikitadhawale <102734279+nikitadhawale@users.noreply.github.com>
2025-07-03 10:36:15 +0000

    new file added

---
+++ 1_basics/1_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12443;
+let accountEmail = "john2@gmail.com";
+var accountPassword = "1234";
+accountCity = "surat";
+accountstate = "";
+
+
+console.log(accountPassword);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountstate]);
+
+
---
+++ 1_basics/2_conversions.js
@@ -0,0 +1,23 @@
+// let score = ture;
+
+// console.log(typeof score);
+
+// console.log(typeof(score));
+
+
+// number will not be not converted to string
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//33 => 33
+//33abc => NaN
+
+
+let isLoggedIn = "";;
+
+let boolenIsLoggedIn = Boolean(isLoggedIn);
+console.log(boolenIsLoggedIn);
+
+
