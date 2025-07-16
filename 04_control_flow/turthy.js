8e4eb3115fe63778e41fda2bc8b1954c6e8e1e42
nikitadhawale <102734279+nikitadhawale@users.noreply.github.com>
2025-07-16 18:15:54 +0000

    truthy values

--- 04_control_flow/turthy.js
+++ 04_control_flow/turthy.js
@@ -1,10 +1,10 @@
 const userEmail = []
 
-if(userEmail){
-    console.log("Got email");
-}  else {
-    console.log("Don't have email");
-}
+// if(userEmail){
+//     console.log("Got email");
+// }  else {
+//     console.log("Don't have email");
+// }
 
 // falsy values
 
@@ -19,9 +19,31 @@ if(userEmail){
 //    console.log("arr is empty");
 // }
 
-const emptyObj = {}
+// const emptyObj = {}
 
-if(Object.keys(emptyObj).length === 0){
-    console.log("object is empty");
-    
-}
+// if(Object.keys(emptyObj).length === 0){
+//     console.log("object is empty"); 
+// }
+
+
+// Nullish Coalescing Operator (??) null undefinde
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+// val1 = null ?? 10 ?? 78
+
+
+// console.log(val1);
+
+
+
+// terniary Operator
+
+// condition ? true : false
+
+const iceTea = 400;
+iceTea <= 80 ? console.log("less than 80") : console.log("more than 80");
+
+
