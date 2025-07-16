bfbb112830026a86e2118a5cac30ad2929a1cbaf
nikitadhawale <102734279+nikitadhawale@users.noreply.github.com>
2025-07-16 17:04:50 +0000

    control flows

---
+++ 04_control_flow/one.js
@@ -0,0 +1,48 @@
+//control flow
+
+//if
+// if(true){
+//     //excute code
+// }
+// if(false){
+//     //not excute
+// }
+
+//<, >, <=, >=, ==, ===, !=, 
+
+// const temp = 41;
+
+// if (temp === 41){
+//     console.log("less than 50");
+// } else {
+//    console.log("greater than 50");
+// }
+// console.log("excuted");
+
+// const score = 200;
+
+// if (score > 100) {
+//     const power = "fly";
+//     console.log(`user power: ${power}`);   
+// }
+
+//no scope for power varible
+// console.log(`user power: ${power}`);   
+
+
+
+//short hand if
+
+const balance = 1000
+
+// //implict scope
+// if(balance > 500) console.log("test"),
+// //multiple add
+// console.log("test2");
+
+//nested if
+
+if( balance < 500){
+  log
+}
+
