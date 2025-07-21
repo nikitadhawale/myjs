e972f0e9f2e3008d70c7636bdef24104575a03d1
nikitadhawale <102734279+nikitadhawale@users.noreply.github.com>
2025-07-21 10:45:26 +0000

    for while do

---
+++ 05_iteration/two.js
@@ -0,0 +1,24 @@
+//while loop
+// let i = 0;
+// while (i <= 10) {
+//     console.log(`value of i is: ${i}`);
+//     i = i + 2;
+// }
+
+// let myArray = ["flash", "batman", "superman"]
+
+// let arr = 0;
+// while(arr < myArray.length){
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+
+//do while
+
+let score = 1;
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
